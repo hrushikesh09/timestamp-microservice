dfdb037d1fd971714c86f0ea160b32245f0d0b6b
hrushikesh09 <hrushikeshshinde11@yahoo.com>
2016-05-02 18:13:50 +0530

    main feature

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var moment = require('moment');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,8 +10,32 @@ app.use(express.static(__dirname + '/public'));
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('index');
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/:date', function(req, res){
+
+  var unixTimeStamp = null, date = null;
+
+  if(moment(req.params.date).isValid()){
+
+    unixTimeStamp = moment(req.params.date).unix();
+    date = req.params.date;
+
+  } else if(!isNaN(moment.unix(req.params.date))){
+
+    unixTimeStamp = req.params.date;
+    date = moment.unix(req.params.date).format("MMMM DD, YYYY");
+  }
+
+  var json = {
+    unixTimeStamp: unixTimeStamp,
+    natural: date
+  }
+
+  res.setHeader('content-type', 'application/json');
+  res.send(json);
 });
 
 app.listen(app.get('port'), function() {
